fc2fa0bf1701b10e386e499660fe1899d7054b97
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-06 22:16:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   index = 0
   squared.each do |number|
     new_squared[index]= squared[index]*squared[index]
-    puts squared[index]
+    puts new_squared[index]
     index += 1
   end
 end
